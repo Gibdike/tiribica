8e78ac383228e7b74d9834c50870beb21262b49a
Lorhan <gabriellorhan0@gmail.com>
2019-09-17 19:26:00 -0400

    V.1.0.0
    Primeiro Commit ;
    Sprites Importados da Unity Assets
    Fundo Parallax Feito
    Movimentação Basica feita

---
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+
+    public float velocidade;
+    private Rigidbody2D rb;
+    public bool podePular;
+    public float forcaPulo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * velocidade,rb.velocity.y);
+        if(rb.velocity.x != 0){
+            GetComponent<Animator>().SetBool("Walking", true);
+        }else{
+            GetComponent<Animator>().SetBool("Walking", false);
+        }
+        if(rb.velocity.x < 0){
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        if(rb.velocity.x > 0){
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        if(Input.GetKeyDown(KeyCode.W) && podePular)
+        {
+            rb.AddForce(new Vector2( 0, forcaPulo));
+            podePular = false;
+            GetComponent<Animator>().SetBool("Jumping", true);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Chao")){
+            GetComponent<Animator>().SetBool("Jumping", false);
+            podePular = true;
+        }       
+    }
+}
---
+++ Assets/Scripts/UI/Paralax.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Paralax : MonoBehaviour
+{
+    public RawImage paralax;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        paralax.uvRect = new Rect(paralax.uvRect.x + 0.001f, paralax.uvRect.y, paralax.uvRect.width, paralax.uvRect.height);
+    }
+}
