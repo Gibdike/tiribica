28d00f1bf36c24b3a98111db1f7f1af485324e7a
Lorhan <gabriellorhan0@gmail.com>
2019-10-03 22:03:49 -0400

    Medidor de freuquencia
    coloquei o medidor de frequencia e mexi no mapa e criei o script de morte

---
+++ Assets/Scripts/Player/Morte.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Morte : MonoBehaviour
+{
+    public PlayerBehaviour player;
+    private void Update() {
+        if (player.vidas < 1){
+            Destroy (gameObject);
+        }
+    }
+
+    
+}
